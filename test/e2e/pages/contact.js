95a921f23e7a57b269937ff4ebb5307eae27fd20
sachinsurpur <sachinsurpur@gmail.com>
2017-03-28 09:32:14 -0400

    Added new page object file for Contact page

---
+++ test/e2e/pages/contact.js
@@ -0,0 +1,10 @@
+module.exports = {
+  elements: {
+    contactLink: 'a[href*="contact"]',
+    contactTitle:  '.mdl-card__title-text',
+    contactInfoName:  '#contactInfoName',
+    contactInfoEmail: '#contactInfoEmail',
+    contactInfoNote:  '#contactInfoNote',
+    submitButton: '.mdl-button'
+  }
+};
